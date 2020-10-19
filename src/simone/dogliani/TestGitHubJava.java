f12d432863846a0aed9d6fe829d865a475a2146f
Simone Dogliani <simone.dogliani585@edu.unito.it>
2020-10-19 10:27:17 +0200

    initial commit

---
+++ src/simone/dogliani/TestGitHubJava.java
@@ -0,0 +1,7 @@
+package simone.dogliani;
+
+public class TestGitHubJava {
+    public static void main(String[] args) {
+        System.out.println("Sono nel main di Test GitHub Simone Dogliani");
+    }
+}
