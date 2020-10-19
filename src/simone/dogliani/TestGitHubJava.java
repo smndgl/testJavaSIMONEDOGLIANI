4070659be1ac1436abd044bfee6b5194fb55bc1d
Simone Valletta <simone.valletta@edu.unito.it>
2020-10-19 11:42:20 +0200

    modifica Valletta

--- src/simone/dogliani/TestGitHubJava.java
+++ src/simone/dogliani/TestGitHubJava.java
@@ -3,5 +3,6 @@ package simone.dogliani;
 public class TestGitHubJava {
     public static void main(String[] args) {
         System.out.println("Sono nel main di Test GitHub Simone Dogliani");
+        System.out.println("sono un hacker nel computer di Dogliani");
     }
 }
